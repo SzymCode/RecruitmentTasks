2a76278db4b0f81ef8cfdf530fd1b40014c2542f
SzymCode <s.radomski19@gmail.com>
2023-05-31 19:28:10 +0200

    Custom form library - Formik

--- DishFormReact/dish-form/src/components/Home.js
+++ DishFormReact/dish-form/src/components/Home.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
-class Home extends Component {
-  constructor(props) {
-    super(props);
+import React from 'react';
+import { useFormik } from 'formik';
 
-    this.state = {
+
+const Home = () => {
+  const formik = useFormik({
+    initialValues: {
       dishName: '',
       preparationTime: '',
       dishType: '',
@@ -11,131 +12,114 @@ class Home extends Component {
       diameter: '',
       spicinessScale: '',
       slicesOfBread: '',
-    };
-  }
+    },
+    onSubmit: (values) => {
+      const dish = {
+        name: values.dishName,
+        preparation_time: values.preparationTime,
+        type: values.dishType,
+        no_of_slices: values.dishType === 'pizza' ? parseInt(values.noOfSlices) : undefined,
+        diameter: values.dishType === 'pizza' ? parseFloat(values.diameter) : undefined,
+        spiciness_scale: values.dishType === 'soup' ? parseInt(values.spicinessScale) : undefined,
+        slices_of_bread: values.dishType === 'sandwich' ? parseInt(values.slicesOfBread) : undefined,
+      };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-
-    const {
-      dishName,
-      preparationTime,
-      dishType,
-      noOfSlices,
-      diameter,
-      spicinessScale,
-      slicesOfBread,
-    } = this.state;
-
-    const dish = {
-      name: dishName,
-      preparation_time: preparationTime,
-      type: dishType,
-      no_of_slices: dishType === 'pizza' ? parseInt(noOfSlices) : undefined,
-      diameter: dishType === 'pizza' ? parseFloat(diameter) : undefined,
-      spiciness_scale: dishType === 'soup' ? parseInt(spicinessScale) : undefined,
-      slices_of_bread: dishType === 'sandwich' ? parseInt(slicesOfBread) : undefined,
-    };
-
-
-    fetch('https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dish),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data); // Show the response data
+      fetch('https://umzzcc503l.execute-api.us-west-2.amazonaws.com/dishes/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dish),
       })
-      .catch((error) => {
-        console.error(error); // Handle errors
-      });
-  };
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data); // Show the response data
+        })
+        .catch((error) => {
+          console.error(error); // Handle errors
+        });
+    },
+  });
 
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+  return (
+    <div id="container-fluid">
+      <h1 id="header">
+        Create a Dish
+      </h1>
+      <form onSubmit={formik.handleSubmit}>
 
-  render() {
-    return (
-        <div id="container-fluid">
-          <h1 id="header">Create a Dish</h1>
-          <form onSubmit={this.handleFormSubmit}>
-
-            <div>
-              <label htmlFor="dishName">
-                Dish Name:
-              </label>
-              <input type="text" id="dishName" name="dishName" value={this.state.dishName} onChange={this.handleInputChange} required/>
-            </div>
-
-            <div>
-              <label htmlFor="preparationTime">
-                Preparation Time (HH:MM:SS):
-              </label>
-              <input type="text" id="preparationTime" name="preparationTime" value={this.state.preparationTime} onChange={this.handleInputChange} required/>
-            </div>
-
-            <div>
-              <label htmlFor="dishType">
-                Dish Type:
-              </label>
-              <select id="dishType" name="dishType" value={this.state.dishType} onChange={this.handleInputChange} required>
-                <option value="">
-                  Select Type
-                </option>
-                <option value="pizza">
-                  Pizza
-                </option>
-                <option value="soup">
-                  Soup
-                </option>
-                <option value="sandwich">
-                  Sandwich
-                </option>
-              </select>
-            </div>
-
-            {this.state.dishType === 'pizza' && (
-                <div>
-                  <label htmlFor="noOfSlices">
-                    Number of Slices:
-                  </label>
-                  <input type="number" id="noOfSlices" name="noOfSlices" value={this.state.noOfSlices} onChange={this.handleInputChange} required/>
-                </div>
-            )}
-            {this.state.dishType === 'pizza' && (
-                <div>
-                  <label htmlFor="diameter">
-                    Diameter:
-                  </label>
-                  <input type="number" step="0.1" id="diameter" name="diameter" value={this.state.diameter} onChange={this.handleInputChange} required/>
-                </div>
-            )}
-            {this.state.dishType === 'soup' && (
-                <div>
-                  <label htmlFor="spicinessScale">
-                    Spiciness Scale (1-10):
-                  </label>
-                  <input type="number" id="spicinessScale" name="spicinessScale" value={this.state.spicinessScale} onChange={this.handleInputChange} min="1" max="10" required/>
-                </div>
-            )}
-            {this.state.dishType === 'sandwich' && (
-                <div>
-                  <label htmlFor="slicesOfBread">
-                    Slices of Bread:
-                  </label>
-                  <input type="number" id="slicesOfBread" name="slicesOfBread" value={this.state.slicesOfBread} onChange={this.handleInputChange} required/>
-                </div>
-            )}
-
-            <button>Submit</button>
-          </form>
+        <div>
+          <label htmlFor="dishName">
+            Dish Name:
+          </label>
+          <input type="text" id="dishName" name="dishName" value={formik.values.dishName} onChange={formik.handleChange} required/>
         </div>
-    );
-  }
-}
+
+        <div>
+          <label htmlFor="preparationTime">
+            Preparation Time (HH:MM:SS):
+          </label>
+          <input type="text" id="preparationTime" name="preparationTime" value={formik.values.preparationTime} onChange={formik.handleChange} required/>
+        </div>
+
+        <div>
+          <label htmlFor="dishType">
+            Dish Type:
+          </label>
+          <select id="dishType" name="dishType" value={formik.values.dishType} onChange={formik.handleChange} required>
+            <option value="">
+              Select Type
+            </option>
+            <option value="pizza">
+              Pizza
+            </option>
+            <option value="soup">
+              Soup
+            </option>
+            <option value="sandwich">
+              Sandwich
+            </option>
+          </select>
+        </div>
+
+        {formik.values.dishType === 'pizza' && (
+          <div>
+            <label htmlFor="noOfSlices">
+              Number of Slices:
+            </label>
+            <input type="number" id="noOfSlices" name="noOfSlices" value={formik.values.noOfSlices} onChange={formik.handleChange} required/>
+          </div>
+        )}
+        {formik.values.dishType === 'pizza' && (
+          <div>
+            <label htmlFor="diameter">
+              Diameter:
+            </label>
+            <input type="number" step="0.1" id="diameter" name="diameter" value={formik.values.diameter} onChange={formik.handleChange} required/>
+          </div>
+        )}
+        {formik.values.dishType === 'soup' && (
+          <div>
+            <label htmlFor="spicinessScale">
+              Spiciness Scale (1-10):
+            </label>
+            <input type="number" id="spicinessScale" name="spicinessScale" value={formik.values.spicinessScale} onChange={formik.handleChange} min="1" max="10" required/>
+          </div>
+        )}
+        {formik.values.dishType === 'sandwich' && (
+          <div>
+            <label htmlFor="slicesOfBread">
+              Slices of Bread:
+            </label>
+            <input type="number" id="slicesOfBread" name="slicesOfBread" value={formik.values.slicesOfBread} onChange={formik.handleChange} required/>
+          </div>
+        )}
+
+        <button>Submit</button>
+
+      </form>
+    </div>
+  );
+};
+
 export default Home;
