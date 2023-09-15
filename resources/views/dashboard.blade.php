1bcf654d77ac81b25214c8b108ef3225fbb37cab
SzymCode <s.radomski19@gmail.com>
2023-09-15 11:38:59 +0200

    AdminPanel: Models and seeders management + migrations
    Initialize dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <div class="users_content card mt-3">
+            <div class="card-body">
+                <h3>Manage Users</h3>
+            </div>
+        </div>
+        <div class="posts_content card mt-3">
+            <div class="card-body">
+                <h3>Manage Posts</h3>
+            </div>
+        </div>
+    </div>
+@endsection
