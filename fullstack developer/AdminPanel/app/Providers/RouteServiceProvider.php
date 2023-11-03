de3209363c231f4e665fd712ef4a3b1779206227
SzymCode <s.radomski19@gmail.com>
2023-11-03 11:10:31 +0100

    AdminPanel: Tenancy tests

--- fullstack developer/AdminPanel/app/Providers/RouteServiceProvider.php
+++ fullstack developer/AdminPanel/app/Providers/RouteServiceProvider.php
@@ -10,6 +10,31 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    protected function mapWebRoutes()
+    {
+        foreach ($this->centralDomains() as $domain) {
+            Route::middleware('web')
+                ->domain($domain)
+                ->namespace($this->namespace)
+                ->group(base_path('routes/web.php'));
+        }
+    }
+
+    protected function mapApiRoutes()
+    {
+        foreach ($this->centralDomains() as $domain) {
+            Route::prefix('api')
+                ->domain($domain)
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+        }
+    }
+
+    protected function centralDomains(): array
+    {
+        return config('tenancy.central_domains', []);
+    }
     /**
      * The path to your application's "home" route.
      *
@@ -29,12 +54,8 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
-
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+            $this->mapApiRoutes();
+            $this->mapWebRoutes();
         });
     }
 }
