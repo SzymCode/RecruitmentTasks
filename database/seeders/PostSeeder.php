a7a7903af1361164456b9b333cb533afc87a7e49
SzymCode <s.radomski19@gmail.com>
2023-11-06 12:31:40 +0100

    AdminPanel: Tenancy tests

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,12 +14,6 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        Post::create([
-            'title' => 'First post',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            'tags' => implode(', ', ['PHP', 'Laravel', 'Vue.js', 'Bootstrap']),
-            'updated_at' => '14.09.2023',
-            'created_at' => '14.09.2023'
-        ]);
+
     }
 }
