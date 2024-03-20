d8b42e844672452c3f2999eff10a0ec440709b9e
szymcode <s.radomski19@gmail.com>
2024-03-20 11:10:22 +0100

    SMSManager: Successfully listed SMS messages + test connection function with info

--- src/Controller/SMSController.php
+++ src/Controller/SMSController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Service\EmailService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,14 +17,35 @@ class SMSController extends AbstractController
         $this->emailService = $emailService;
     }
 
+    /**
+     *  API Routes
+     */
+    #[Route("/api/sms/test")]
+    public function testConnectionWithInfo(): JsonResponse | Response
+    {
+        return $this->emailService->testConnectionWithInfo();
+    }
     #[Route("/api/sms")]
     public function fetchAllSMS(): Response
     {
         return $this->emailService->fetchAndSaveSMS('ALL');
     }
-    #[Route("/api/unseen-sms")]
+    #[Route("/api/sms/unseen")]
     public function fetchUnseenSMS(): Response
     {
         return $this->emailService->fetchAndSaveSMS('UNSEEN');
+    }
+
+    /**
+     *  Render routes
+     */
+    #[Route("/sms/list")]
+    public function listSMS(): Response
+    {
+        $smsList = $this->fetchAllSMS();
+
+        return $this->render('sms/list.html.twig', [
+            'smsList' => $smsList,
+        ]);
     }
 }
