a7e884ed360d7db86f31ea402ab61340b1cfaa7b
SzymCode <s.radomski19@gmail.com>
2024-03-27 21:09:22 +0100

    LinkhouseBlog: PrimeIcons installed + navbar improvements
    More Sass variables and styles

--- fullstack developer/LinkhouseBlog/resources/js/components/MyNavbar.vue
+++ fullstack developer/LinkhouseBlog/resources/js/components/MyNavbar.vue
@@ -1,9 +1,29 @@
 <template>
-    <div class="myNav">
-        <div class="container myNavContainer">
+    <nav class="navbar navbar-expand-md">
+        <div class="container container-fluid">
             <img src="../../../public/logo.png" alt="logo" class="logo" />
 
-            <a href="/" class="nav-link"> Articles </a>
+            <Button
+                class="navbar-toggler"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+            >
+                <i class="pi pi-bars navbarTogglerIcon" />
+            </Button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/"> Articles </a>
+                    </li>
+                </ul>
+                <form class="searchForm" id="searchForm">
+                    <InputText class="searchInput" placeholder="Search..." />
+                    <i class="pi pi-search searchIcon" />
+                </form>
+            </div>
         </div>
-    </div>
+    </nav>
 </template>
