6292e93cfcf0bb963454abb3140160ad12e2d093
SzymCode <s.radomski19@gmail.com>
2024-03-28 22:28:59 +0100

    LinkhouseBlog: Complete search functionality + switch search by title or category

--- fullstack developer/LinkhouseBlog/resources/js/components/Articles.vue
+++ fullstack developer/LinkhouseBlog/resources/js/components/Articles.vue
@@ -4,23 +4,31 @@
             <template #header>
                 <div class="headerContainer">
                     <h2>Articles</h2>
-
-                    <form class="searchForm" id="searchForm">
+                    <form
+                        class="searchForm"
+                        id="searchForm"
+                        @submit.prevent="searchArticles"
+                    >
                         <InputText
                             class="searchInput"
+                            v-model="searchTerm"
                             placeholder="Search..."
                         />
-                        <i class="pi pi-search searchIcon" />
+                        <Dropdown
+                            :options="searchOptions"
+                            v-model="selectedSearchOption"
+                            class="searchDropdown"
+                        />
                     </form>
                 </div>
             </template>
             <template #content>
                 <DataTable
-                    :value="articles"
+                    :value="filteredArticles"
                     :rows="10"
                     :row-hover="true"
                     :size="'small'"
-                    v-if="articles"
+                    v-if="filteredArticles"
                     paginator
                     stripedRows
                     paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
@@ -58,9 +66,39 @@
 
 <script setup lang="ts">
 import { articleApiMethods } from '../utils'
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
+import { ArticleInterface } from '@/js/types'
 
-const { results: articles, getAllArticles } = articleApiMethods()
+const { results: fetchedArticles, getAllArticles } = articleApiMethods()
 
-onMounted(getAllArticles)
+const searchTerm = ref('')
+const articles = ref<ArticleInterface[]>([])
+const filteredArticles = ref<ArticleInterface[]>([])
+
+const searchOptions = ['Title', 'Category']
+let selectedSearchOption = ref(searchOptions[0])
+
+onMounted(async () => {
+    await getAllArticles()
+    if (fetchedArticles.value) {
+        articles.value = fetchedArticles.value
+        filteredArticles.value = fetchedArticles.value
+    }
+})
+
+function searchArticles() {
+    if (!searchTerm.value.trim()) {
+        filteredArticles.value = articles.value
+        return
+    }
+
+    const searchField =
+        selectedSearchOption.value === 'Title' ? 'title' : 'category'
+
+    filteredArticles.value = articles.value.filter((article) =>
+        article[searchField]
+            .toLowerCase()
+            .includes(searchTerm.value.toLowerCase())
+    )
+}
 </script>
