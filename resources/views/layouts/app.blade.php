e9b45b7f0a37952109785b99868f9d091b70fe12
SzymCode <s.radomski19@gmail.com>
2024-06-10 20:56:35 +0200

    AdminPanel: (AFTER TIME) Updated favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>AdminPanel</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
