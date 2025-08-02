a0eff6cea2b6750a6293b47f6c54bd8d41161a07
SzymCode <szymon.radomski@yahoo.com>
2025-08-02 18:08:16 +0200

    PatientManager: Moved Nuxt config files to root folder
    Integrated Nuxt with Laravel via routes/web.php

--- nuxt/components/welcome/index.vue
+++ nuxt/components/welcome/index.vue
@@ -2,7 +2,7 @@
   <section id="welcome">
     <div class="welcome-container">
       <a href="https://github.com/Atomic-IT">
-        <img src="/logo.png" alt="Atomic IT logo" width="150" height="150" />
+        <img src="/public/logo.png" alt="Atomic IT logo" width="150" height="150" />
         <h1>Nuxt Starter</h1>
       </a>
     </div>
