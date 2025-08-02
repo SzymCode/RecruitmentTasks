4ade8553a0777e7babb6d64f2b771aee4274917e
SzymCode <szymon.radomski@yahoo.com>
2025-08-02 22:13:59 +0200

    PatientManager: Uninstalled unnecessary dependencies

--- fullstack developer/PatientManager/nuxt/app.vue
+++ fullstack developer/PatientManager/nuxt/app.vue
@@ -5,14 +5,7 @@
   </div>
 </template>
 
-<script setup lang="ts">
-defineOgImageComponent('NuxtSeo', {
-  title: 'NuxtStarter by Atomic IT',
-  description:
-    'This template offers a fully integrated development workflow, designed to streamline your project setup and enhance code quality.',
-  colorMode: 'dark',
-})
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss">
 @use 'styles';
